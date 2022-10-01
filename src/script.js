75691ca8f014e11d925e9791cd1b9426870eb795
Igson <igson660@gmail.com>
2022-10-01 15:19:05 -0500

    :sparkles: style scrollbar

--- src/script.js
+++ src/script.js
@@ -18,25 +18,46 @@ const fullWeeks = [
   "Domingo",
 ];
 
+//insert weekdays in select
 fullWeeks.forEach((week) =>
   utils.insertContent("option", week, "#select-weeks")
 );
 
+//insert buttons weekdays
 fullWeeks.forEach((week) =>
   utils.insertContent("button", week, "#buttons-group", false, "button-weekday")
 );
 
+//remove local storage
 $selector("#delete-storage").addEventListener("click", () =>
-  console.log("teste")
+  storage.removeItem("tasks")
 );
 
+//add in local storages
 $selector("#save-storage").addEventListener("click", () =>
   console.log("teste")
 );
 
+//add and remove active class in buttons week days
 $selectorAll(".button-weekday").forEach((button) =>
   button.addEventListener("click", (e) => {
     utils.removeClassActive();
     button.classList.add("active");
   })
 );
+
+//add task
+$selector("#add-task").addEventListener("click", (e) => {
+  e.preventDefault();
+  const task = $selector("#input-task").value;
+  const weekday = $selector("#select-weeks").value;
+  const hours = $selector("#select-hours").value;
+
+  console.log(task, weekday, hours);
+});
+
+//remove task
+$selector("#delete-task").addEventListener("click", (e) => {
+  e.preventDefault();
+  console.log("button delete");
+});
